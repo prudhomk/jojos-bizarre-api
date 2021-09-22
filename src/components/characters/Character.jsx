34450fe8599e135ecc5d0a20bf99ca417e60f26f
Katherine Tam <katherinemtam@gmail.com>
2021-09-22 16:04:38 -0700

    Character component

---
+++ src/components/characters/Character.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Character({ abilities, catchphrase, chapter, family, id, image, isHuman, japaneseName, living, name, nationality }) {
+  return (
+    <>
+      <p>Name: {name}</p>
+      <p>Japanese Name: {japaneseName}</p>
+      <p>{image}</p>
+      <p>Catchphrase: {catchphrase}</p>
+      <p>Nationality: {nationality}</p>
+      <p>Abilities: {abilities}</p>
+      <p>Chapter: {chapter}</p>
+      <p>Family: {family}</p>
+      <p>IsHuman: {isHuman.toString()}</p>
+      <p>Living: {living.toString()}</p>
+    </>
+  );
+}
