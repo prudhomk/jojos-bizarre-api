8e2138cb3d7c76b46933cd0eac14072b000d0530
Katherine Tam <katherinemtam@gmail.com>
2021-09-22 16:04:08 -0700

    add Characters route

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import CharacterList from '../characters/CharacterList';
 import Home from '../home/Home';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <h1>Hello World</h1>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/characters" component={CharacterList}/>
       </Switch>
     </>
   );
