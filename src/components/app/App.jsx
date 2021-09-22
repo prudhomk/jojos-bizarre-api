b0dbf41e266c51196a44f8d671fe6bb5a1eef665
Katherine Tam <katherinemtam@gmail.com>
2021-09-22 15:40:38 -0700

    add Home route

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../home/Home';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
+    </>
+  );
 }
