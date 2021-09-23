5442b2a5d698fd6fbf1a9cc63e1731dd537b8169
Katherine Tam <katherinemtam@gmail.com>
2021-09-22 17:04:11 -0700

    add CharacterDetails rroute

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import CharacterList from '../characters/CharacterList';
 import StandList from '../stands/StandList';
 import Home from '../home/Home';
+import CharacterDetails from '../characters/CharacterDetails';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route exact path="/characters" component={CharacterList}/>
+        <Route exact path="/characters/:id" component={CharacterDetails}/>
         <Route exact path="/stands" component={StandList}/>
       </Switch>
     </>
