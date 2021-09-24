36b411c31c85316289562d4e8dc52b18396fa9fd
Katherine Tam <katherinemtam@gmail.com>
2021-09-24 15:37:29 -0700

    delete character functionality + routing buttons

--- src/components/characters/CharacterDetails.jsx
+++ src/components/characters/CharacterDetails.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router';
+import { deleteCharacter } from '../../services/jojoApi';
 import { useCharacter } from '../../state/customHooks';
 
 export default function CharacterDetails() {
   const { id } = useParams();
-
   const character = useCharacter(id);
+  const history = useHistory();
+
+  const handleEdit = async () => {
+    history.push(`/characters/edit/${id}`);
+  };
+
+  const handleDelete = async () => {
+
+    if(window.confirm('Are you sure you want to delete this character?')) {
+      await deleteCharacter(id);
+      history.push('/characters');
+    }
+
+  };
+
+  const handleReturn = async () => {
+    history.push('/characters');
+  };
   
   return (
     <div>
@@ -19,6 +38,10 @@ export default function CharacterDetails() {
       <p>Family: {character.family}</p>
       <p>IsHuman: {JSON.stringify(character.isHuman)}</p>
       <p>Living: {JSON.stringify(character.living)}</p>
+
+      <button onClick={handleEdit}>Edit Character</button>
+      <button onClick={handleDelete}>Delete Character</button>
+      <button onClick={handleReturn}>View All Characters</button>
     </div>
   );
 }
