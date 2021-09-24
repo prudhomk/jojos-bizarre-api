e2b1d9caa04d6f831ada17df32d025ebd834041a
Katherine Tam <katherinemtam@gmail.com>
2021-09-24 15:37:46 -0700

    Add Character Button on characters page

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import { useCharacters } from '../../state/customHooks';
 import Character from './Character';
 
 const CharacterList = () => {
   const { characters } = useCharacters();
+  const history = useHistory();
 
+  const handleAdd = () => {
+    history.push('/characters/add');
+  };
+  
   const characterElements = characters.map(character => (
     <li key={character.id}>
       <Character {...character} />
@@ -12,7 +18,12 @@ const CharacterList = () => {
   ));
 
 
-  return <h1>{characterElements}</h1>;
+  return (
+    <>
+      <button onClick={handleAdd}>Add Character</button>
+      <h1>{characterElements}</h1>;
+    </>
+  );
 };
 
 export default CharacterList;
