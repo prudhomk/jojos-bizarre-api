df1ba8dfeebcb745cb3f7a2651b9dead2493591e
Kalan Prudhomme <cleaver9@gmail.com>
2021-10-11 15:09:09 -0700

    added loader, adjusted queries table and styling

--- src/state/customHooks.js
+++ src/state/customHooks.js
@@ -5,16 +5,16 @@ import { fetchCharacters, fetchCharacter, fetchStands, fetchStand } from '../ser
 export const useCharacters = (page) => {
   const [characters, setCharacters] = useState([]);
   const [charLength, setCharLength] = useState([]);
-
+  const [loader, setLoader] = useState(true);
   useEffect(async () => {
 
     const res = await fetchCharacters(page);
     setCharLength(res.length);
     setCharacters(res.slice((page - 1) * 15, page * 15));
+    setLoader(false);
+  }, [page, loader]);
 
-  }, [page]);
-
-  return { characters, charLength };
+  return { characters, charLength, loader };
 };
 
 export const useCharacter = (id) => {
@@ -30,16 +30,19 @@ export const useCharacter = (id) => {
   return character;
 };
 
-export const useStands = () => {
+export const useStands = (page) => {
   const [stands, setStands] = useState([]);
-
-  useEffect(() => {
+  const [stanLength, setStanLength] = useState([]);
+  const [loader, setLoader] = useState(true);
+  useEffect(async () => {
     
-    fetchStands()
-      .then(setStands);
-  }, []);
+    const res = await fetchStands(page);
+    setStanLength(res.length);
+    setStands(res.slice((page - 1) * 15, page * 15));
+    setLoader(false);
+  }, [page, loader]);
 
-  return { stands };
+  return { stands, stanLength, loader };
 };
 
 export const useStand = (id) => {
